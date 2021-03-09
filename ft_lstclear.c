a94a1331cd5a8976abf317825ef10fa9da887ccc
RealMadnessWorld <76601093+RealMadnessWorld@users.noreply.github.com>
2021-03-09 14:47:44 +0000

    Add files via upload

---
+++ ft_lstclear.c
@@ -0,0 +1,16 @@
+
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	if (!lst)
+		return ;
+	while (*lst)
+	{
+		temp = *lst->next;
+		ft_lstdelone(lst, del);
+		*lst = temp;
+	}
+	*lst = NULL;
+}
